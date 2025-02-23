d6fd4e67002dfb650cb86795515d459e62c3e9b0
tanmaygupta069 <tanmaygupta1102@gmail.com>
2025-02-23 22:08:18 +0530

    chore: styled not-found page

--- src/components/Not-Found/Not-Found.tsx
+++ src/components/Not-Found/Not-Found.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, Typography } from '@mui/material';
 
-const NotFound = ()=>{
+const NotFound = () => {
     return (
         <Box sx={{
             pt: '64px',
@@ -10,7 +10,7 @@ const NotFound = ()=>{
             textAlign: 'center',
             alignItems: 'center',
             py: { xs: 4, md: 8 },
-            justifyContent:'center'
+            justifyContent: 'center'
 
         }}>
             <Container maxWidth="md" sx={{ px: { xs: 2, md: 3 }, textAlign: 'center', alignItems: 'center' }}>
@@ -22,6 +22,16 @@ const NotFound = ()=>{
                         padding: '12px 24px',
                         mb: { xs: 3, md: 4 },
                         fontSize: { xs: '2rem', sm: '3rem', md: '4.5rem' },
+                        // fontWeight: 'bold',
+                        color: 'white', // Black text
+                        // WebkitTextStroke: '1px rgba(0, 0, 0, 0.5)', 
+                        textShadow: `
+                    2px 2px 2px rgba(0, 0, 0, 0.3),
+                    4px 4px 4px rgba(0, 0, 0, 0.3),
+                    6px 6px 6px rgba(0, 0, 0, 0.3),
+                    8px 8px 8px rgba(0, 0, 0, 0.3),
+                    10px 10px 10px rgba(0, 0, 0, 0.3)
+                    `
                     }}
                 >
                     404
@@ -34,7 +44,7 @@ const NotFound = ()=>{
                         fontSize: { xs: '1.1rem', sm: '1.3rem', md: '1.5rem' },
                         lineHeight: { xs: 1.5, md: 1.7 },
                     }}
-                >The page you are looking for does not exist 
+                >The page you are looking for does not exist
                 </Typography>
             </Container>
         </Box>
